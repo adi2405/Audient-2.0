51e315a24a87d4520f3d695134722acdf91fc73f
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-04-09 10:49:47 +0530

    05: ai voice assistant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
 import { Button } from "@workspace/ui/components/button";
+import { useVapi } from "@/features/widget/hooks/use-vapi";
 
 export default function Page() {
+  const {
+    isSpeaking,
+    isConnecting,
+    isConnected,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi();
+
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
@@ -13,6 +25,16 @@ export default function Page() {
         <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
         </div>
+        <div className="flex gap-x-4">
+          <Button onClick={() => startCall()}>Start call</Button>
+          <Button variant={"destructive"} onClick={() => endCall()}>
+            End call
+          </Button>
+        </div>
+        <p>Is Connected: {`${isConnected}`}</p>
+        <p>Is Connecting: {`${isConnecting}`}</p>
+        <p>Is Speaking: {`${isSpeaking}`}</p>
+        <p>{JSON.stringify(transcript, null, 2)}</p>
       </div>
     </div>
   );
