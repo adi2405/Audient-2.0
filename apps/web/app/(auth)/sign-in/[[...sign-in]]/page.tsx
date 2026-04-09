5e82ca5c4f7d2c30ee87d5d871800558dfe7630b
adi2405 <144616497+adi2405@users.noreply.github.com>
2026-04-09 10:11:55 +0530

    04: organizations

--- apps/web/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ apps/web/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignInView } from "@/features/auth/views/sign-in-view";
 
 export default function Page() {
-  return <SignIn />;
+  return <SignInView />;
 }
